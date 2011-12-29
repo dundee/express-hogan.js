01f34c406d2058fa572b84da76831cb00508c721
Daniel Milde <daniel@milde.cz>
2011-12-29 01:07:16 +0100

    initial

---
+++ testapp.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var expressHogan = require('./lib/express-hogan');
+
+var app = module.exports = express.createServer();
+
+app.configure(function() {
+	app.set('views', __dirname + '/views');
+	app.register('.html', expressHogan);
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	return app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function() {
+	return app.use(express.errorHandler({
+		dumpExceptions: true,
+		showStack: true
+	}));
+});
+
+app.configure('production', function() {
+	return app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res) {
+	return res.render('index.html', {
+		locals: {
+			title: 'Express test',
+			message: 'using Hogan templating system'
+		}
+	});
+});
+
+app.listen(3000);
+
+console.log('Express server listening on port 3000');
